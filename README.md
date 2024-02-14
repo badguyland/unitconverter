ac18c6ad57dc7a88b54ecc41a58f5fe6b61195bc
fcrt <you@example.com>
2024-02-14 15:07:32 +0000

    added time conversion

---
+++ README.md
@@ -0,0 +1,31 @@
+# Unit Converter
+
+## Description
+
+This is a unit converter which uses a PySimpleGUI window. 
+![](images/screenshot1.png)
+You input the value you want to change into it , choose the type of conversion, such as volume, and choose between which measurements it should convert.
+
+## How it works
+
+The program takes data from the dictionary `unit_conversions` .
+
+## Notes
+
+It is advised to use PySimpleGUI 4.60.5 as Version 5.0.0, needs user registration or payment.
+ 
+
+## Future work
+
+- more conversion units
+- better asthetics
+
+
+
+
+
+
+
+
+
+
--- converter.py
+++ converter.py
@@ -17,13 +17,27 @@ unit_conversions = {
         "Grams": {"Pounds": 0.00220462, "Ounces": 0.035274, "Kilograms": 0.001}
     },
     "Volume": {
-    "Liters": {"Milliliters": 1000, "Gallons": 0.264172, "Quarts": 1.05669, "Pints": 2.11338},  # Added Pints conversion factor
-    "Milliliters": {"Liters": 0.001, "Gallons": 0.000264172, "Quarts": 0.00105669, "Pints": 0.00211338},
-    "Gallons": {"Liters": 3.78541, "Milliliters": 3785.41, "Quarts": 4, "Pints": 8},             # Added Pints conversion factor
-    "Quarts": {"Liters": 0.946353, "Milliliters": 946.353, "Gallons": 0.25, "Pints": 0.5},        # Added Pints conversion factor
-    "Pints": {"Liters": 0.473176, "Milliliters": 473.176, "Gallons": 0.125, "Quarts": 2}           # Added Pints conversion factor
+        "Liters": {"Milliliters": 1000, "Gallons": 0.264172, "Quarts": 1.05669, "Pints": 2.11338},
+        "Milliliters": {"Liters": 0.001, "Gallons": 0.000264172, "Quarts": 0.00105669, "Pints": 0.00211338},
+        "Gallons": {"Liters": 3.78541, "Milliliters": 3785.41, "Quarts": 4, "Pints": 8},
+        "Quarts": {"Liters": 0.946353, "Milliliters": 946.353, "Gallons": 0.25, "Pints": 0.5},
+        "Pints": {"Liters": 0.473176, "Milliliters": 473.176, "Gallons": 0.125, "Quarts": 2}
+    },
+    "Time": {
+        "Nanoseconds": {"Microseconds": 0.001, "Milliseconds": 1e-6, "Seconds": 1e-9, "Minutes": 1.66667e-11, "Hours": 2.77778e-13, "Days": 1.15741e-14, "Weeks": 1.65344e-15, "Months": 3.80518e-16, "Years": 3.16888e-17},
+        "Microseconds": {"Nanoseconds": 1000, "Milliseconds": 0.001, "Seconds": 1e-6, "Minutes": 1.66667e-8, "Hours": 2.77778e-10, "Days": 1.15741e-11, "Weeks": 1.65344e-12, "Months": 3.80518e-13, "Years": 3.16888e-14},
+        "Milliseconds": {"Nanoseconds": 1e6, "Microseconds": 1000, "Seconds": 0.001, "Minutes": 1.66667e-5, "Hours": 2.77778e-7, "Days": 1.15741e-8, "Weeks": 1.65344e-9, "Months": 3.80518e-10, "Years": 3.16888e-11},
+        "Seconds": {"Nanoseconds": 1e9, "Microseconds": 1e6, "Milliseconds": 1000, "Minutes": 0.0166667, "Hours": 2.77778e-4, "Days": 1.15741e-5, "Weeks": 1.65344e-6, "Months": 3.80518e-7, "Years": 3.16888e-8},
+        "Minutes": {"Nanoseconds": 6e10, "Microseconds": 6e7, "Milliseconds": 60000, "Seconds": 60, "Hours": 0.0166667, "Days": 0.000694444, "Weeks": 9.9206e-5, "Months": 2.28311e-5, "Years": 1.90259e-6},
+        "Hours": {"Nanoseconds": 3.6e12, "Microseconds": 3.6e9, "Milliseconds": 3.6e6, "Seconds": 3600, "Minutes": 60, "Days": 0.0416667, "Weeks": 0.00595238, "Months": 0.00136986, "Years": 0.000114155},
+        "Days": {"Nanoseconds": 8.64e13, "Microseconds": 8.64e10, "Milliseconds": 8.64e7, "Seconds": 86400, "Minutes": 1440, "Hours": 24, "Weeks": 0.142857, "Months": 0.0328767, "Years": 0.00273973},
+        "Weeks": {"Nanoseconds": 6.048e14, "Microseconds": 6.048e11, "Milliseconds": 6.048e8, "Seconds": 604800, "Minutes": 10080, "Hours": 168, "Days": 7, "Months": 0.229985, "Years": 0.019165},
+        "Months": {"Nanoseconds": 2.63e15, "Microseconds": 2.63e12, "Milliseconds": 2.63e9, "Seconds": 2.63e6, "Minutes": 43829.1, "Hours": 730.484, "Days": 30.4368, "Weeks": 4.34524, "Years": 0.0833333},
+        "Years": {"Nanoseconds": 3.15e16, "Microseconds": 3.15e13, "Milliseconds": 3.15e10, "Seconds": 3.15e7, "Minutes": 525600, "Hours": 8760, "Days": 365, "Weeks": 52.1429, "Months": 12}
+    }
 }
-}
+
+
 
 def update_unit_options(window, selected_type):
     from_options = list(unit_conversions[selected_type].keys())
@@ -46,6 +60,7 @@ while True:
     event, values = window.read()
 
     if event == sg.WIN_CLOSED or event == 'Quit':
+        print ("Shutting down...")
         break
 
     if event == 'Convert':
