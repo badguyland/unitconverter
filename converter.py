02625cbd74c495e0a299d46408916574adab8ef0
fcrt <you@example.com>
2024-02-20 21:55:11 +0000

    added base conversion

--- converter.py
+++ converter.py
@@ -1,6 +1,5 @@
 import PySimpleGUI as sg
 
-
 unit_conversions = {
     "Length": {
         "Inches": {"Centimetres": 2.54, "Feet": 1/12, "Yards": 1/36, "Metres": 0.0254, "KiloMetres": 0.0000254, "Miles": 1.57828e-5},
@@ -37,24 +36,45 @@ unit_conversions = {
     }
 }
 
-
-
 def update_unit_options(window, selected_type):
     from_options = list(unit_conversions[selected_type].keys())
     window['-FROM-'].update(values=from_options)
     window['-TO-'].update(values=from_options)
 
-layout = [
+def convert_base(num, base):
+    digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    stack = []
+    while num > 0:
+        stack.append(digits[num % base])
+        num //= base
+    result = ""
+    while stack:
+        result += stack.pop()
+    return result
+
+unit_converter_layout = [
     [sg.Text('Enter value:'), sg.InputText(key = '-INPUT-')],
     [sg.Text('Type:'), sg.Combo(list(unit_conversions.keys()), key='-KIND-', enable_events=True)],
     [sg.Text('From:'), sg.Combo([], key='-FROM-', size=(20, 20))],
     [sg.Text('To:'), sg.Combo([], key='-TO-', size=(20, 20))],
     [sg.Button('Convert')],
     [sg.Text('Converted value:'), sg.Text(size=(15,1), key='-OUTPUT-')],
-    [sg.Button('Quit')] 
+    [sg.Button('Quit')]
 ]
 
-window = sg.Window('Unit Converter', layout)
+base_converter_layout = [
+    [sg.Text('Enter a number:'), sg.Input(key='number')],
+    [sg.Text('From base:'), sg.Input(key='from_base')],
+    [sg.Text('To base:'), sg.Input(key='to_base')],
+    [sg.Button('Convert')],
+    [sg.Text(size=(40, 1), key='output')]
+]
+
+tab_layout = [
+    [sg.TabGroup([[sg.Tab('Unit Converter', unit_converter_layout), sg.Tab('Base Converter', base_converter_layout)]])]
+]
+
+window = sg.Window('Converter', tab_layout)
 
 while True:
     event, values = window.read()
@@ -65,17 +85,22 @@ while True:
 
     if event == 'Convert':
         try:
-            value = float(values['-INPUT-'])
-            conversion_type = values['-KIND-']
-            from_unit = values['-FROM-']
-            to_unit = values['-TO-']
-            if from_unit == to_unit:
-                converted_value = value
+            if 'number' in values:
+                num = int(values['number'], int(values['from_base']))
+                result = convert_base(num, int(values['to_base']))
+                window['output'].update(f"Result: {result}")
             else:
-                conversion_factor = unit_conversions[conversion_type][from_unit][to_unit]
-                converted_value = value * conversion_factor
-            window['-OUTPUT-'].update(converted_value)
-        except (ValueError, KeyError) :
+                value = float(values['-INPUT-'])
+                conversion_type = values['-KIND-']
+                from_unit = values['-FROM-']
+                to_unit = values['-TO-']
+                if from_unit == to_unit:
+                    converted_value = value
+                else:
+                    conversion_factor = unit_conversions[conversion_type][from_unit][to_unit]
+                    converted_value = value * conversion_factor
+                window['-OUTPUT-'].update(converted_value)
+        except (ValueError, KeyError):
             sg.Popup("Input Error") 
 
     if event == '-KIND-':
